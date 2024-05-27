09a9a3aec53437edc089888d6dd810261355f63b
Ethan Walker <ethandavidwalker@gmail.com>
2024-05-27 15:10:43 -0500

    stored frontend in public folder and changed luanch settings

--- public/formHandler.js
+++ public/formHandler.js
@@ -3,9 +3,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     window.handlePassword = function(event) {
         event.preventDefault(); // Prevent the form from submitting normally
-
-        var password = document.getElementById('password').value;
-        var correctPassword = 'a'; // Replace with the actual correct password
+        
+        let password = document.getElementById('password').value;
+        let correctPassword = 'a'; // Replace with the actual correct password
 
         if (password === correctPassword) {
             window.location.href = 'secret recipe.htm'; // Redirect to the secret recipe page
